b9e06f4c927155b920cdfa566af087997be1cb93
Fabian Seluska <fabianseluska@gmail.com>
2021-02-12 13:50:59 +0100

    Dark mode added (works). Some fixes for panels.

--- assets/panels.js
+++ assets/panels.js
@@ -6,14 +6,25 @@ document.querySelectorAll(".panel-btn").forEach(button =>
 
 closeBtn = document.querySelector("#close-btn")
 
-closeBtn.addEventListener("click", _ => {
-    activePanel = document.querySelector(`#${closeBtn.getAttribute("activePanel")}`)
-    activePanel.style.left = activePanel.getAttribute("direction") == "right" ? "110vw" : "-50vw"
-    closeBtn.style.left = "110vw"
-})
+closeBtn.addEventListener("click", closePanel)
 
 function openPanel(panel, direction) {
+    // Close any previous panels
+    activePanel = document.querySelector(`#${closeBtn.getAttribute("activePanel")}`)
+    if (activePanel)
+        activePanel.style.left = activePanel.getAttribute("direction") == "right" ? "110vw" : "-50vw"
+
+    // Open the new one
     document.querySelector(panel).style.left = direction == "right" ? "60vw" : "0vw"
-    closeBtn.style.left = direction == "right" ? "56.5vw" : "38.5vw"
+    closeBtn.style.left = direction == "right" ? "57vw" : "39vw"
     closeBtn.setAttribute("activePanel", document.querySelector(panel).id)
+}
+
+function closePanel() {
+    activePanel = document.querySelector(`#${closeBtn.getAttribute("activePanel")}`)
+
+    if (activePanel)
+        activePanel.style.left = activePanel.getAttribute("direction") == "right" ? "110vw" : "-50vw"
+
+    closeBtn.style.left = "110vw"
 }
