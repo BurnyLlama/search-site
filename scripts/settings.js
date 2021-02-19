785783cdd128207c4e739797581ccd111d190cb1
Fabian Seluska <fabianseluska@gmail.com>
2021-02-19 14:26:17 +0100

    Tweaked settings. Added ability to choose colors.
    ALso added abiility to revert to default settings.

--- scripts/settings.js
+++ scripts/settings.js
@@ -17,6 +17,18 @@ function loadTheme(theme) {
     }
 }
 
+function getDefaultSettings() {
+    return {
+        theme: "light",
+        searchEngine: "google",
+        searchEngineCustomURL: "",
+        colors: {
+            color1: "#ff4b4b",
+            color2: "#4b4bff",
+        }
+    }
+}
+
 export default {
     init: () => {
         document.querySelector("#settings-theme").addEventListener("click", event => {
@@ -25,31 +37,65 @@ export default {
             loadTheme(themeSetting.value)
         })
 
+        document.querySelector("#settings-colors").addEventListener("change", () => {
+            setvar("color1", document.querySelector("#color1").value)
+            setvar("color2", document.querySelector("#color2").value)
+        })
+
         document.querySelector("#settings-save-btn").addEventListener("click", event => {
             const settings = {
                 theme: document.querySelector('input[name="theme"]:checked').value,
                 searchEngine: document.querySelector('input[name="search-engine"]:checked').value,
-                searchEngineCustomURL: document.querySelector("#search-engine-custom-url").value
+                searchEngineCustomURL: document.querySelector("#search-engine-custom-url").value,
+                colors: {
+                    color1: document.querySelector("#color1").value,
+                    color2: document.querySelector("#color2").value
+                }
             }
 
             localStorage.setItem("settings", JSON.stringify(settings))
 
             window.location.reload()
         })
+
+        document.querySelector("#settings-default-btn").addEventListener("click", event => {
+            const settings = getDefaultSettings()
+            localStorage.setItem("settings", JSON.stringify(settings))
+
+            window.location.reload()
+        })
     },
+
     loadSettings: () => {
-        const settings = JSON.parse(localStorage.getItem("settings"))
+        let settings = JSON.parse(localStorage.getItem("settings"))
 
+        // Default settings if no settings are present.
         if (settings == null)
-            return console.log("Couldn't load any settings! (There are no settings saved.)")
+            settings = getDefaultSettings()
 
+        // Default theme
         if (settings.theme == null)
-            return console.log("Couldn't load any settings! (There is no theme in the settings!)")
+            settings.theme = "light"
+
         loadTheme(settings.theme)
         document.querySelector(`#${settings.theme}-theme[name="theme"]`).checked = true
 
+        // Default colors
+        if (settings.colors == null)
+            settings.colors = {
+                color1: "#ff4b4b",
+                color2: "#4b4bff",
+            }
+
+        document.querySelector("#color1").value = settings.colors.color1
+        document.querySelector("#color2").value = settings.colors.color2
+        setvar("color1", settings.colors.color1)
+        setvar("color2", settings.colors.color2)
+
+        // Default search engine
         if (settings.searchEngine == null)
-            return console.log("Couldn't load any settings! (There is search engine theme in the settings!)")
+            searchEngine = "google"
+
         document.querySelector(`#search-engine-${settings.searchEngine}[name="search-engine"]`).checked = true
 
         document.querySelector("#search-engine-custom-url").value = settings.searchEngineCustomURL ? settings.searchEngineCustomURL : ""
